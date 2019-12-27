2f3eef3033673143def2ff2c694bc30eaf8321b3
liuvasconcelos <livia.vasconcelos@stant.com.br>
2019-12-27 11:14:07 -0300

    develop Add textures to earth node.
    -
    
    Signed-off-by: liuvasconcelos <livia.vasconcelos@stant.com.br>

--- Planets/ViewController.swift
+++ Planets/ViewController.swift
@@ -7,14 +7,44 @@
 //
 
 import UIKit
+import ARKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var sceneView: ARSCNView!
+    
+    let configuration = ARWorldTrackingConfiguration()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        sceneView.debugOptions = [ARSCNDebugOptions.showFeaturePoints,
+                                  ARSCNDebugOptions.showWorldOrigin]
+        sceneView.session.run(configuration)
+        sceneView.autoenablesDefaultLighting = true
     }
 
-
+    override func viewDidAppear(_ animated: Bool) {
+        let earth = SCNNode()
+        earth.geometry = SCNSphere(radius: 0.2)
+        earth.geometry?.firstMaterial?.diffuse.contents  = UIImage(named: "Earth")
+        earth.geometry?.firstMaterial?.specular.contents = UIImage(named: "Earth Specular")
+        earth.geometry?.firstMaterial?.emission.contents = UIImage(named: "Earth Clouds")
+        earth.geometry?.firstMaterial?.normal.contents   = UIImage(named: "Earth Normal")
+        earth.position = SCNVector3(0, 0, -1)
+        
+        sceneView.scene.rootNode.addChildNode(earth)
+        
+        let action = SCNAction.rotateBy(x: 0,
+                                        y: CGFloat(360.degreesToRadians),
+                                        z: 0,
+                                        duration: 8)
+        let forever = SCNAction.repeatForever(action)
+        earth.runAction(forever)
+    }
 }
 
+extension Int {
+    var degreesToRadians: Double { return Double(self) * .pi/180}
+}
